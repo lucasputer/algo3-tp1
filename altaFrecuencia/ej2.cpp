02f0d93845bdf7674dbf0778467dd489cf977528
Ivan Arcuschin <iarcuschin@gmail.com>
2015-03-26 11:35:20 -0300

    Re-indento ej 2

--- altaFrecuencia/ej2.cpp
+++ altaFrecuencia/ej2.cpp
@@ -5,16 +5,16 @@ using namespace std;
 
 // Varios typedefs
 
-  struct Signal
-  {
-  	Signal() : numero(0), costo(0), principio(0), fin(0) {};
+struct Signal
+{
+    Signal() : numero(0), costo(0), principio(0), fin(0) {};
     Signal(const int n,const int c,const int p, const int f) : numero(n), costo(c), principio(p), fin(f) {};
 
     int numero;
     int costo;
     int principio;
     int fin;
-  };
+};
 
 typedef vector<Signal> Vec;
 
@@ -32,36 +32,36 @@ void mostrarPrueba(const Vec& v);
  */
 int main()
 {
-	int n;
+    int n;
 
-	while(true) {
-		cin >> n;
-		if(cin.eof()){
-			break;
-		}
+    while(true) {
+        cin >> n;
+        if(cin.eof()){
+            break;
+        }
 
-		Vec l(n);
+        Vec l(n);
 
-		for(int i = 0; i < n; ++i)
-		{
-			int j=0;
-			int values [3];
-			while(j<3)
-			{
-				cin >> values[j];
-				j++;
-			}
-			l[i] = Signal(i+1,values[0],values[1],values[2]);
+        for(int i = 0; i < n; ++i)
+        {
+            int j=0;
+            int values [3];
+            while(j<3)
+            {
+                cin >> values[j];
+                j++;
+            }
+            l[i] = Signal(i+1,values[0],values[1],values[2]);
 
-		}
+        }
 
-		l = mergeSort(l);
+        l = mergeSort(l);
 
 
-		mostrar(l);
-	}
+        mostrar(l);
+    }
 
-	return 0;
+    return 0;
 }
 
 Vec mergeSort(vector<Signal>& vec)
@@ -71,20 +71,20 @@ Vec mergeSort(vector<Signal>& vec)
     {
         return vec;
     }
- 
-    
+
+
     Vec::iterator medio = vec.begin() + (vec.size() / 2);
- 
-   Vec izq(vec.begin(), medio);
-   Vec der(medio, vec.end());
+
+    Vec izq(vec.begin(), medio);
+    Vec der(medio, vec.end());
 
     // mostrarPrueba( izq);
     // cout << ">>>>>>>>>>>";
     // mostrarPrueba( der);
 
     // cout << "------------";
- 
-    
+
+
     izq = mergeSort(izq);
     der = mergeSort(der);
 
@@ -95,36 +95,36 @@ Vec merge(Vec &vec, Vec& izq,  Vec& der)
 {
     Vec result;
     int izq_it = 0, der_it = 0;
- 
+
     while(izq_it < izq.size() && der_it < der.size())
     {
         if(izq[izq_it].principio <= der[der_it].principio)
         {
-        	mergeAux(izq,der,izq_it,der_it,result);
+            mergeAux(izq,der,izq_it,der_it,result);
         }
         else
         {
             mergeAux(der,izq,der_it,izq_it,result);
         }
     }
- 
+
     while(izq_it < izq.size())
     {
         result.push_back(izq[izq_it]);
         izq_it++;
     }
- 
+
     while(der_it < der.size())
     {
         result.push_back(der[der_it]);
         der_it++;
     }
-	vec = result;	
-	// cout << endl;
-	// cout << "***********";
-	// mostrarPrueba(vec);	
-	// cout << "***********";		
-	return vec;
+    vec = result;	
+    // cout << endl;
+    // cout << "***********";
+    // mostrarPrueba(vec);	
+    // cout << "***********";		
+    return vec;
 }
 
 /**
@@ -132,85 +132,85 @@ Vec merge(Vec &vec, Vec& izq,  Vec& der)
  */
 void mostrar(const Vec& v)
 {
-	cout << endl;
-	int n = v.size();
-	int finalCost = 0;
-	for(int i = 0; i < n; ++i)
-	{
-		finalCost += v[i].costo * (v[i].fin - v[i].principio);
-	}
-	cout << finalCost;
-	cout << endl;
+    cout << endl;
+    int n = v.size();
+    int finalCost = 0;
+    for(int i = 0; i < n; ++i)
+    {
+        finalCost += v[i].costo * (v[i].fin - v[i].principio);
+    }
+    cout << finalCost;
+    cout << endl;
 
-	for(int i = 0; i < n; ++i)
-	{
-		cout << v[i].numero;
-		cout << " " << v[i].principio;
-		cout << " " << v[i].fin;
-		
-		cout << endl;
-	}
+    for(int i = 0; i < n; ++i)
+    {
+        cout << v[i].numero;
+        cout << " " << v[i].principio;
+        cout << " " << v[i].fin;
+
+        cout << endl;
+    }
 }
 
 void mostrarPrueba(const Vec& v)
 {
-	cout << endl;
-	int n = v.size();
+    cout << endl;
+    int n = v.size();
 
-	for(int i = 0; i < n; ++i)
-	{
-		cout << v[i].numero;
-		cout << " " << v[i].costo;
-		cout << " " << v[i].principio;
-		cout << " " << v[i].fin;
-		
-		cout << endl;
-	}
+    for(int i = 0; i < n; ++i)
+    {
+        cout << v[i].numero;
+        cout << " " << v[i].costo;
+        cout << " " << v[i].principio;
+        cout << " " << v[i].fin;
+
+        cout << endl;
+    }
 }
 
 void mergeAux(Vec& menor, Vec& mayor, int& i, int& j,Vec& result)
 {
     bool flag = false;
     if(menor[i].costo <= mayor[j].costo)
+    {
+        if(mayor[j].principio <= menor[i].fin)
         {
-            if(mayor[j].principio <= menor[i].fin)
-            {
-                mayor[j].principio = menor[i].fin;
-            }
-            if(mayor[j].principio >= mayor[j].fin)
-            {
-                j++;
-                flag = true;
-            }
-            if(!flag)
-            {
-                result.push_back(menor[i]);
-                i++;
-            }
-            else
-            {
-                flag = false;
-            }
+            mayor[j].principio = menor[i].fin;
+        }
+        if(mayor[j].principio >= mayor[j].fin)
+        {
+            j++;
+            flag = true;
+        }
+        if(!flag)
+        {
+            result.push_back(menor[i]);
+            i++;
         }
         else
         {
-            int aux = menor[i].fin;
-            if(mayor[j].principio <= menor[i].fin)
-            {
-                menor[i].fin = mayor[j].principio;
-            }
-            if(menor[i].fin > menor[i].principio)
-            {
-                result.push_back(menor[i]);
-            }
-            if(mayor[j].fin < aux)
-            {
-                menor[i].principio = mayor[j].fin;
-                menor[i].fin = aux;                  
-            }
-            else
-            {
-                i++;
-            }
+            flag = false;
         }
+    }
+    else
+    {
+        int aux = menor[i].fin;
+        if(mayor[j].principio <= menor[i].fin)
+        {
+            menor[i].fin = mayor[j].principio;
+        }
+        if(menor[i].fin > menor[i].principio)
+        {
+            result.push_back(menor[i]);
+        }
+        if(mayor[j].fin < aux)
+        {
+            menor[i].principio = mayor[j].fin;
+            menor[i].fin = aux;                  
+        }
+        else
+        {
+            i++;
+        }
+    }
 }
