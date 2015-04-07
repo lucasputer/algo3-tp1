e3922a4dc9594d1d15f6fab6d6f8b447dbf90ca9
Jose <jmmassigoge@gmail.com>
2015-04-07 18:48:19 -0300

    Pruebas

--- Zombieland/files/instancias.cpp
+++ Zombieland/files/instancias.cpp
@@ -7,11 +7,11 @@ using namespace std;
 
 int main() {
 
-	for (int i = 1; i <= 1; i++) {
+	for (int i = 1; i <= 800; i++) {
     		char buffer[32];
-    		snprintf(buffer, sizeof(char) * 32, "file%i.txt", i);
+    		snprintf(buffer, sizeof(char) * 32, "file%i.in", i);
 		FILE* file = fopen(buffer,"a+");
-		int n = rand() % 10000 + 1; // 0 < n < 10001
+		int n = rand() % 100000 + 1; // 0 < n < 10001
         	int P = rand() % 10000; // sin restricción P
 		fprintf(file, "%d %d\n", n, P);
         	while (n > 0) { // genero la instancia
